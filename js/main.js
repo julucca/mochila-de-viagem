c18aad462b94ae82187ce045acf0930f98d80b01
julucca <lucca.editora@gmail.com>
2023-02-25 14:45:08 -0300

    feat: create functionality to remove list items

--- js/main.js
+++ js/main.js
@@ -24,10 +24,10 @@ form.addEventListener('submit', (evento) => {
 
         atualizaElemento(itemAtual);
 
-        itens[existe.id] = itemAtual;
+        itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual;
 
     } else {
-        itemAtual.id = itens.length;
+        itemAtual.id = itens[itens.length - 1] ? itens[itens.length - 1].id + 1 : 0;
 
         criaElemento(itemAtual);
     
@@ -47,15 +47,37 @@ function criaElemento(item) {
     novoItem.classList.add('item');
 
     const numeroItem = document.createElement('strong');
-    numeroItem.innerHTML = item.quantidade
-    numeroItem.dataset.id = item.id
+    numeroItem.innerHTML = item.quantidade;
+    numeroItem.dataset.id = item.id;
 
-    novoItem.appendChild(numeroItem)
-    novoItem.innerHTML += item.nome
+    novoItem.appendChild(numeroItem);
+    novoItem.innerHTML += item.nome;
+
+    novoItem.appendChild(botaoDeleta(item.id));
 
     lista.appendChild(novoItem);
 }
 
 function atualizaElemento(item) {
    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade;
+}
+
+function botaoDeleta(id) {
+    const elementoBotao = document.createElement("button");
+    elementoBotao.innerText = "X";
+
+    // cria evento para o botão
+    elementoBotao.addEventListener('click', function() {
+        deletaElemento(this.parentNode, id)
+    }); 
+
+    return elementoBotao
+}
+
+function deletaElemento(tag, id) {
+    tag.remove()
+
+    itens.splice(itens.findIndex(elemento => elemento.id === id), 1);
+
+    localStorage.setItem('itens', JSON.stringify(itens));
 }
